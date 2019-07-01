cbdc317e6c0019d7d0186a2a9362f7f8398a6c37
Alex Glen <alex@boclips.com>
2019-07-01 12:41:48 +0100

    Add toString to BoclipsApiException
    This provides valuable debugging/tracing information for when things go
    wrong.
    
    Signed-off-by: Benjamin Gröhbiel <ben@boclips.com>

--- src/main/kotlin/com/boclips/web/exceptions/BoclipsApiException.kt
+++ src/main/kotlin/com/boclips/web/exceptions/BoclipsApiException.kt
@@ -1,12 +1,15 @@
 package com.boclips.web.exceptions
 
 import org.springframework.http.HttpStatus
-import java.lang.RuntimeException
 
-abstract class BoclipsApiException(val exceptionDetails: ExceptionDetails) : RuntimeException()
+abstract class BoclipsApiException(val exceptionDetails: ExceptionDetails) : RuntimeException() {
+    override fun toString(): String {
+        return "${exceptionDetails.error}: ${exceptionDetails.message}"
+    }
+}
 
 data class ExceptionDetails(
-        val error: String,
-        val message: String,
-        val status: HttpStatus = HttpStatus.BAD_REQUEST
+    val error: String,
+    val message: String,
+    val status: HttpStatus = HttpStatus.BAD_REQUEST
 )
